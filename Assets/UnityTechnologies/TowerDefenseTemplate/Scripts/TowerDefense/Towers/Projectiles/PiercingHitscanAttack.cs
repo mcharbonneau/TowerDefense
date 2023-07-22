be0900ddc24e976df00847aa2e51f40553247bd9
Maxime Charbonneau <max@parabole.ca>
2023-07-22 14:11:28 -0400

    Piercing laser tower modifier

---
+++ Assets/UnityTechnologies/TowerDefenseTemplate/Scripts/TowerDefense/Towers/Projectiles/PiercingHitscanAttack.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using ActionGameFramework.Health;
+using Core.Utilities;
+using UnityEngine;
+
+namespace TowerDefense.Towers.Projectiles
+{
+	/// <summary>
+	/// Implementation of hitscan projectile
+	/// The principle behind this weapon is that it instantly attacks enemies
+	/// </summary>
+	[RequireComponent(typeof(Damager))]
+	public class PiercingHitscanAttack : MonoBehaviour
+	{
+		/// <summary>
+		/// The amount of time to delay
+		/// </summary>
+		public float delay;
+
+		/// <summary>
+		/// The delay timer
+		/// </summary>
+		protected Timer m_Timer;
+
+		/// <summary>
+		/// The enemies this projectile will attack
+		/// </summary>
+		protected IList<Targetable> m_Enemies;
+
+		/// <summary>
+		/// The Damager attached to the object
+		/// </summary>
+		protected Damager m_Damager;
+
+		/// <summary>
+		/// Configuration for pausing the timer delay timer
+		/// without setting Time.timeScale to 0
+		/// </summary>
+		protected bool m_PauseTimer;
+
+		/// <summary>
+		/// The delay configuration for the attacking
+		/// </summary>
+		/// <param name="origin">
+		/// The point the attack will be fired from
+		/// </param>
+		/// <param name="enemies">
+		/// The enemies to attack
+		/// </param>
+		public void AttackEnemies(IList<Targetable> enemies)
+		{
+			m_Enemies = enemies;
+			
+			m_Timer.Reset();
+			m_PauseTimer = false;
+		}
+
+		/// <summary>
+		/// The actual attack of the piercing hitscan attack.
+		/// Early returns from the method if the there is no enemy to attack.
+		/// </summary>
+		protected void DealDamage()
+		{
+			Poolable.TryPool(gameObject);
+			
+			foreach (var enemy in m_Enemies)
+			{
+				if (enemy == null)
+				{
+					continue;
+				}
+				
+				// effects
+				ParticleSystem pfxPrefab = m_Damager.collisionParticles;
+				var attackEffect = Poolable.TryGetPoolable<ParticleSystem>(pfxPrefab.gameObject);
+				attackEffect.transform.position = enemy.position;
+				attackEffect.Play();
+			
+				enemy.TakeDamage(m_Damager.damage, enemy.position, m_Damager.alignmentProvider);
+			}
+			
+			m_PauseTimer = true;
+		}
+
+		/// <summary>
+		/// Cache the damager component attached to this object
+		/// </summary>
+		protected virtual void Awake()
+		{
+			m_Damager = GetComponent<Damager>();
+			m_Timer = new Timer(delay, DealDamage);
+		}
+
+		/// <summary>
+		/// Update the m_Timer if it is available
+		/// </summary>
+		protected virtual void Update()
+		{
+			if (!m_PauseTimer)
+			{
+				m_Timer.Tick(Time.deltaTime);
+			}
+		}
+	}
+}
---
+++ Assets/UnityTechnologies/TowerDefenseTemplate/Scripts/TowerDefense/Towers/TowerLaunchers/PiercingHitscanLauncher.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using ActionGameFramework.Health;
+using TowerDefense.Targetting;
+using TowerDefense.Towers.Projectiles;
+using UnityEngine;
+
+namespace TowerDefense.Towers.TowerLaunchers
+{
+	/// <summary>
+	/// An implementation of the tower launcher for piercing hitscan attacks
+	/// </summary>
+	public class PiercingHitscanLauncher : Launcher
+	{
+		/// <summary>
+		/// The particle system used for providing launch feedback
+		/// </summary>
+		[SerializeField] private ParticleSystem m_FireParticleSystem;
+		
+		[SerializeField] private float m_CapsuleHalfHeight = 2f;
+		[SerializeField] private float m_CapsuleRadius = 0.1f;
+		
+		/// <summary>
+		/// Custom max distance for the attack, since it can go further than the tower's targeting range
+		/// </summary>
+		[SerializeField] private float m_Distance = 20;
+		
+		[SerializeField] private LayerMask m_LayerMask;
+		
+		// Reuse the same arrays to avoid allocations
+		private RaycastHit[] m_Hits = new RaycastHit[256];
+		private List<Targetable> m_Targetables = new List<Targetable>();
+		
+		/// <summary>
+		/// Assigns the correct damage to the hitscan object and every enemy in the line of fire.
+		/// </summary>
+		/// <param name="enemy">
+		/// The enemy this tower is targeting
+		/// </param>
+		/// <param name="attack">
+		/// The attacking component used to damage the enemy
+		/// </param>
+		/// <param name="firingPoint"></param>
+		public override void Launch(Targetable enemy, GameObject attack, Transform firingPoint)
+		{
+			var hitscanAttack = attack.GetComponent<PiercingHitscanAttack>();
+			if (hitscanAttack == null)
+			{
+				return;
+			}
+			
+			var origin = firingPoint.position;
+			var targetPosition = enemy.position;
+			targetPosition.y = origin.y;
+			
+			RunCapsuleCast(targetPosition, origin);
+			
+			hitscanAttack.transform.position = origin;
+			hitscanAttack.AttackEnemies(m_Targetables);
+			
+			PlayParticles(m_FireParticleSystem, origin, targetPosition);
+		}
+
+		private void RunCapsuleCast(Vector3 targetPosition, Vector3 origin)
+		{
+			var direction = Vector3.Normalize(targetPosition - origin);
+
+			var capsuleSide = Vector3.up * m_CapsuleHalfHeight;
+			
+			var point1 = origin + capsuleSide;
+			var point2 = origin - capsuleSide;
+
+			var count = Physics.
+				CapsuleCastNonAlloc(point1, point2, m_CapsuleRadius, direction, m_Hits, m_Distance, m_LayerMask.value);
+			
+			FillTargetablesList(count);
+		}
+
+		private void FillTargetablesList(int count)
+		{
+			m_Targetables.Clear();
+			
+			for (int i = 0; i < count; i++)
+			{
+				var hit = m_Hits[i];
+				var targetable = hit.collider.GetComponent<Targetable>();
+				if (targetable != null)
+				{
+					m_Targetables.Add(targetable);
+				}
+			}
+		}
+	}
+}
